3223968fc03355940f5cb3084e355707abaa5c3b
kuan lyu <46909761+kuanlv@users.noreply.github.com>
2019-11-26 21:03:16 -0500

    11/26

--- DJANGO/project/listings/views.py
+++ DJANGO/project/listings/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .choices import price_choices, bedroom_choices, state_choices
 from .models import listing 
 
 def index(request):
@@ -16,7 +17,49 @@ def index(request):
     return render(request, 'listings/listings.html', context)
 
 def _listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    Listing = get_object_or_404(listing, pk=listing_id)
+
+    context = {
+        'listing': Listing
+    }
+
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+    queryset_list = listing.objects.order_by('-list_date')
+
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords)
+
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city)
+
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            queryset_list = queryset_list.filter(state__iexact=state)
+
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)
+
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte=price) 
+
+
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'listings': queryset_list,
+        'values': request.GET
+    }
+
+    return render(request, 'listings/search.html', context)
--- DJANGO/project/pages/views.py
+++ DJANGO/project/pages/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from listings.models import listing
+from listings.choices import price_choices, bedroom_choices, state_choices
 from realtors.models import Realtor
 
 def index(request):
     listings = listing.objects.order_by('-list_date').filter(is_published=True)[:3]
 
     context = {
-        'listings': listings 
+        'listings': listings,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices
     }
     return render(request, 'pages/index.html', context)
 
